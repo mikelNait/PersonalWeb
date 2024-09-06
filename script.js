c36c45cb0c2b17a3abfffd9348386241ac4d62b9
MikelNait <mig_guerre@hotmail.com>
2024-09-06 14:49:48 +0200

    primera version seria de la web personal

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const cargado = () =>  {
+
+
+const karroGrande = document.querySelector('.karro-grande');
+const puntoKarro = document.querySelectorAll('.punto-karro');
+
+
+puntoKarro.forEach((punto , i)=> {
+    puntoKarro[i].addEventListener('click', ()=>{
+        
+        let posicionPunto = i;
+        let widthImg = 100 / puntoKarro.length
+        let operacionPunto = posicionPunto * -widthImg
+        console.log(operacionPunto)
+        karroGrande.style.transform = `translateX(${operacionPunto}%)`
+
+        puntoKarro.forEach((punto, i) => {
+            puntoKarro[i].classList.remove('activo')
+        })
+        puntoKarro[i].classList.add('activo')
+    })
+})
+
+}
+
